ca6b88d886b4fcc73c4b5e0c5ef178ae3dbc89db
藤岡正哉 <mac12140321@icloud.com>
2021-01-22 14:10:19 +0900

    NextViewControllerを作成してViewと紐付けをしました。 M.F

---
+++ Github1/Controller/NextViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NextViewController.swift
+//  Github1
+//
+//  Created by 藤岡正哉 on 2021/01/22.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+    
+
+}
